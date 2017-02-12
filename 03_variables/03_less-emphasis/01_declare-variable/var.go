036cda5334cbe8241b16167696ba4e822fa31ea8
Taoist Bear <originaltaoistbear@gmail.com>
2017-02-12 10:07:22 -0600

    demonstrating basic variable assignment

---
+++ 03_variables/03_less-emphasis/01_declare-variable/var.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// simple variable declaration, assignment to type and initialization along with printing using the variable name.
+func main() {
+	var message string
+	message = "Hello World."
+	fmt.Println(message)
+}
