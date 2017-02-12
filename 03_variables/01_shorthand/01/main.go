61bae8982d5bd91cfa4adbdeb5fe58aa769da971
Taoist Bear <originaltaoistbear@gmail.com>
2017-02-12 10:00:26 -0600

    updating comments

--- 03_variables/01_shorthand/01/main.go
+++ 03_variables/01_shorthand/01/main.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
+// using basic variables with shorthand method and printing their value
 func main() {
 	a := 10
 	b := "golang"
 	c := 4.17
 	d := true
 
+	// %v will format for standard outpout for variable by type
 	fmt.Printf("%v \n", a)
 	fmt.Printf("%v \n", b)
 	fmt.Printf("%v \n", c)
--- 03_variables/01_shorthand/02/main.go
+++ 03_variables/01_shorthand/02/main.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
+// using variables with shorthand method and printing their type
 func main() {
 	a := 10
 	b := "golang"
 	c := 4.17
 	d := true
 
+	// %T will format for type
 	fmt.Printf("%T \n", a)
 	fmt.Printf("%T \n", b)
 	fmt.Printf("%T \n", c)
