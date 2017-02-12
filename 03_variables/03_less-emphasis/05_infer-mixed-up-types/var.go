cad5edcb057ea86c78468fc15d2639502d5d483b
Taoist Bear <originaltaoistbear@gmail.com>
2017-02-12 10:19:20 -0600

    inferring mixed types works but not reccomended

---
+++ 03_variables/03_less-emphasis/05_infer-mixed-up-types/var.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// delcaring and infering mixed types... not reccomended for readability
+func main() {
+
+	var message = "Hello World!"
+	var a, b, c = 1, false, 3
+
+	fmt.Println(message, a, b, c)
+}
