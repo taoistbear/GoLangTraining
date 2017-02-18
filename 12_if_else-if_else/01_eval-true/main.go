76db6a4e3a82b2a1415419bb912d06c807cc1fe6
Taoist Bear <originaltaoistbear@gmail.com>
2017-02-18 10:00:15 -0600

    added some notes

--- 12_if_else-if_else/01_eval-true/main.go
+++ 12_if_else-if_else/01_eval-true/main.go
@@ -14,3 +14,7 @@ func main() {
 }
 
 // Will need to wait for lecture to identify what is singaling true for this
+
+// Ok.. I get it.  I feel like a dork. All if statements just look for something that
+// evaluates to true. True evaluates to true... that one prints. Wow.  I can't believe
+// I didn't pick up on that right away.  Ok.... moving on...
