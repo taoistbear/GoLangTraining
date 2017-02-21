ccb9e289bbe72cdf3944e15720fb7b0d80b14a70
Taoist Bear <originaltaoistbear@gmail.com>
2017-02-21 09:56:39 -0600

    more notes on scope and pass-by-value

--- 14_functions/15_pasing-by-value/01_int/main.go
+++ 14_functions/15_pasing-by-value/01_int/main.go
@@ -16,3 +16,15 @@ func changeMe(z int) {
 
 // when changeMe is called on line 8
 // the vallue 44 is being passed as an agrument
+
+/*
+Passing by value is referncing arguments in functions. When passing variables
+into functions they do not neccessarily have the same scope as the variable
+being passed. With that being said they only receive the value of said variable.
+
+The only difference would be an function expression within the same func
+declaration that would have access to said variable in scope or if the variable
+was declared at the package level.
+
+This technique should dramatically help keep scope tight.
+*/
