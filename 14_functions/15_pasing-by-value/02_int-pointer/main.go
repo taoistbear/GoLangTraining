dad12ac26363be891a0fe0a44ff0fee39a75de1b
Taoist Bear <originaltaoistbear@gmail.com>
2017-02-21 09:53:24 -0600

    adding notes on pointers and memory address

--- 14_functions/15_pasing-by-value/02_int-pointer/main.go
+++ 14_functions/15_pasing-by-value/02_int-pointer/main.go
@@ -19,3 +19,19 @@ func changeMe(z *int) {
 	fmt.Println(z)  // 0x......
 	fmt.Println(*z) // 24
 }
+
+/*
+Using the pointer you are able to change a variable that is in a DIFFERENT SCOPE.
+Important concetp to learn.
+
+When declaring TYPE in PARAMETER using '*' indicates that it is pointer of reference.
+---- func changeMe(z *int) {}
+
+When declaring a VARIABLE using '*' dereferences that memory address allowing
+access to the the actual value (if not you would be changing the memory address)
+---- *z = 24
+
+When passing AGRUMENT using '&' indicates this is the memory address and creates
+the pointer to be used by other funcs outside of the nomral scope
+---- changeMe(&age)
+*/
