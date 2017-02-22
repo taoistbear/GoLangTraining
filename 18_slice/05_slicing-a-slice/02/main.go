a18afbd70cde82ae890456b4ce196027eb75e5de
Taoist Bear <originaltaoistbear@gmail.com>
2017-02-22 14:03:39 -0600

    notes on indexing slices and arrays

--- 18_slice/05_slicing-a-slice/02/main.go
+++ 18_slice/05_slicing-a-slice/02/main.go
@@ -14,11 +14,11 @@ func main() {
 	}
 
 	fmt.Print("[1:2] ")
-	fmt.Println(greeting[1:2])
+	fmt.Println(greeting[1:2]) // start at index 1 upto but not including 2
 	fmt.Print("[:2] ")
-	fmt.Println(greeting[:2])
+	fmt.Println(greeting[:2]) // start at index 0 upto but not including 2
 	fmt.Print("[5:] ")
-	fmt.Println(greeting[5:])
+	fmt.Println(greeting[5:]) // start at index 5 and all the way through the end
 	fmt.Print("[:] ")
-	fmt.Println(greeting[:])
+	fmt.Println(greeting[:]) // start at index 0 and all the way through the end
 }
