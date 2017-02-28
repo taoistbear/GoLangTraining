c716a6632996b2845c36262ef0909a71ef8830d1
Taoist Bear <originaltaoistbear@gmail.com>
2017-02-28 11:53:22 -0600

    most of next lecture rond typed up

---
+++ 21_interfaces/02_package-sort/02_sort-names_type-String-Slice/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	s := []string{"Zeno", "John", "Al", "Jenny"}
+	fmt.Println(s)
+	sort.StringSlice(s).Sort()
+	// sort.Sort(sort.StringSlice(s))
+	fmt.Println(s)
+}
