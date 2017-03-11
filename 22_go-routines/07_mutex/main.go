8f9cee5e56f4550d219f7bdf35c06426745217d8
Taoist Bear <originaltaoistbear@gmail.com>
2017-03-11 10:08:03 -0600

    added an extra note

--- 22_go-routines/07_mutex/main.go
+++ 22_go-routines/07_mutex/main.go
@@ -25,12 +25,15 @@ func main() {
 func incrementor(s string) {
 	for i := 0; i < 20; i++ {
 		time.Sleep(time.Duration(rand.Intn(20)) * time.Millisecond)
-		// Lock() clamps down the variable and prevents other threads from accessing it
-		mutex.Lock()
-		counter++
-		fmt.Println(s, i, "Counter:", counter)
-		// Unlock() opens up the variable in order allow other threads to access it
-		mutex.Unlock()
+		// some people put a mutex lock inside a separte set of curly braces to describe the lock
+		{
+			// Lock() clamps down the variable and prevents other threads from accessing it
+			mutex.Lock()
+			counter++
+			fmt.Println(s, i, "Counter:", counter)
+			// Unlock() opens up the variable in order allow other threads to access it
+			mutex.Unlock()
+		}
 	}
 	wg.Done()
 }
